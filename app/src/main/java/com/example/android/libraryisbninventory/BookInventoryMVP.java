128455897eeaca3be9fe52a49c1009b1357ffbf8
brownmanrick <ncastro878@gmail.com>
2017-12-02 01:36:01 -0600

    added mvp tutorial link

--- app/src/main/java/com/example/android/libraryisbninventory/BookInventoryMVP.java
+++ app/src/main/java/com/example/android/libraryisbninventory/BookInventoryMVP.java
@@ -5,6 +5,8 @@ import android.view.View;
 
 /**
  * Created by nick on 12/2/2017.
+ * This tutorial used to convert RecyclerView and Adapter into MVP format
+ * https://android.jlelse.eu/recyclerview-in-mvp-passive-views-approach-8dd74633158
  */
 
 public interface BookInventoryMVP {
